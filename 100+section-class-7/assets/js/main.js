e15ee716d4f920527ae371e0f6471a142c5aceaf
mahdia7 <mahdiayasmin7800@gmail.com>
2017-06-08 09:39:40 +0600

    100+class7

---
+++ 100+section-class-7/assets/js/main.js
@@ -0,0 +1,23 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+		$('.staff-lists').owlCarousel({
+			items: 4,
+			autoplay: false,
+			margin: 50,
+			loop: true,
+			nav: true,
+			navText: ["<i class='fa fa-long-arrow-left'></i>", "<i class='fa fa-long-arrow-right'></i>"]
+		});
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
