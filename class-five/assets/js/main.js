99b482df952d0e5c82890664f3a09b44171127f6
mahdia7 <mahdiayasmin7800@gmail.com>
2017-06-10 15:27:03 +0600

    class5

---
+++ class-five/assets/js/main.js
@@ -0,0 +1,41 @@
+(function ($) {
+	"use strict";
+
+	jQuery(document).ready(function($){
+    	$('.case-studies-carousel').owlCarousel({
+			items: 3,
+			margin: 30,
+			nav: false,
+			dots: true,
+			loop: true,
+			autoplay: true,
+			smartSpeed: 2000
+		});
+		
+		$('.testimonial-carousel').owlCarousel({
+			animateIn: 'fadeOutDown',
+			animateOut: 'fadeInUp',
+			items: 1,
+			dots: true,
+			nav: false,
+			loop: true,
+			autoplay: true,
+			smartSpeed: 450
+		});
+		$('.logo-carousel').owlCarousel({
+			items: 6,
+			dots: false,
+			nav: false,
+			loop: true,
+			autoplay: true,
+			smartSpeed: 450
+		});
+    });
+
+    jQuery(window).load(function(){
+
+	
+    });
+
+
+}(jQuery));	
