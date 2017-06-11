d878926c9630cf5552cf80805ce2c2a87f11ddfb
mahdia7 <mahdiayasmin7800@gmail.com>
2017-06-11 10:08:22 +0600

    class21

---
+++ 100+section-class-21/assets/js/main.js
@@ -0,0 +1,23 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+		
+		$('.homepage-slides').owlCarousel({
+			items: 1,
+			dots: true,
+			nav: false,
+			autoplay: true,
+			loop: true,
+			smartSpeed: 2000
+		});
+	});
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
