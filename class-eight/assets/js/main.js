aeeba6d37a48543d3d4b5ed04476f382e9797698
mahdia7 <mahdiayasmin7800@gmail.com>
2017-06-18 18:03:15 +0600

    8

--- class-eight/assets/js/main.js
+++ class-eight/assets/js/main.js
@@ -1,40 +1,12 @@
 (function ($) {
 	"use strict";
 
-	jQuery(document).ready(function($){
-    	$('.case-studies-carousel').owlCarousel({
-			items: 3,
-			margin: 30,
-			nav: false,
-			dots: true,
-			loop: true,
-			autoplay: true,
-			smartSpeed: 2000
-		});
-		
+    jQuery(document).ready(function($){
 		$('.testimonial-carousel').owlCarousel({
-			animateIn: 'fadeOutDown',
-			animateOut: 'fadeInUp',
-			items: 1,
-			dots: true,
-			nav: false,
-			loop: true,
-			autoplay: true,
-			smartSpeed: 450
-		});
-		$('.logo-carousel').owlCarousel({
-			items: 6,
-			dots: false,
-			nav: false,
-			loop: true,
-			autoplay: true,
-			smartSpeed: 450
-		});
-		$('.testimonial-carousel-2').owlCarousel({
 			animateIn: 'zoomIn',
 			animateOut: 'slideOutDown',
 			items: 1,
-			dots: false,
+			dots: true,
 			nav: false,
 			loop: true,
 			autoplay: true,
@@ -43,9 +15,10 @@
 		});
     });
 
+
     jQuery(window).load(function(){
 
-	
+        
     });
 
 
