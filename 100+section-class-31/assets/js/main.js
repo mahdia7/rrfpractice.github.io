81a64f7c21d3ba6f4f436247299843913487bd85
mahdia7 <mahdiayasmin7800@gmail.com>
2017-06-19 15:31:07 +0600

    class31

---
+++ 100+section-class-31/assets/js/main.js
@@ -0,0 +1,21 @@
+(function ($) {
+	"use strict";
+
+	jQuery(document).ready(function ($) {
+
+		var s = skrollr.init();
+		
+		$('.play-btn').magnificPopup({
+			type: 'video'
+		});
+		
+	});
+
+
+	jQuery(window).load(function () {
+
+
+	});
+
+
+}(jQuery));
