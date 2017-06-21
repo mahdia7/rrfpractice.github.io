868599503462601b83722d646b8ed511f8696cef
mahdia7 <mahdiayasmin7800@gmail.com>
2017-06-21 16:32:07 +0600

    class38

---
+++ 100+section-class-38/assets/js/main.js
@@ -0,0 +1,24 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+		
+		$('.homepage-slides').owlCarousel({
+			items: 1,
+			nav: true,
+			dots: true,
+			loop: true,
+			autoplay: true,
+			navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+			animateOut: 'fadeOut'
+		});
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
