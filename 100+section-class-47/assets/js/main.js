60662672732d83a8f9c6bfa1b9dee18cfa070f21
mahdia7 <mahdiayasmin7800@gmail.com>
2017-06-23 17:53:02 +0600

    47

---
+++ 100+section-class-47/assets/js/main.js
@@ -0,0 +1,23 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+		$('.testimonial-carousel').owlCarousel({
+			items: 1,
+			loop: true,
+			nav: false,
+			dots: true,
+			autoplay: true,
+			animateIn: 'slideInDown',
+			animateOut: 'fadeOutDown'
+		})
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
