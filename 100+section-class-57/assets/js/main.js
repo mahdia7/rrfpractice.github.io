ab23e936afa363bc48fad8b5eddeaa9f8f83fa51
mahdia7 <mahdiayasmin7800@gmail.com>
2017-06-27 02:32:26 +0600

    57

---
+++ 100+section-class-57/assets/js/main.js
@@ -0,0 +1,16 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+		
+		$('.testimonial-author-list').perfectScrollbar();
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
