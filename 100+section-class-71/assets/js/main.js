dd835719d4e734d9f238638f44ec4d605ec9bbaf
mahdia7 <mahdiayasmin7800@gmail.com>
2017-07-26 22:10:54 +0600

    71

---
+++ 100+section-class-71/assets/js/main.js
@@ -0,0 +1,21 @@
+(function ($) {
+	"use strict";
+
+	jQuery(document).ready(function ($) {
+		
+		$('[data-toggle="tooltip"]').tooltip();
+		$('[data-toggle="popover"]').popover();
+		
+
+	});
+
+
+	jQuery(window).load(function () {
+
+		$("#purchase").modal("show", "toggle");
+
+	});
+
+
+
+})(jQuery);
