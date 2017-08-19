c69ea2e6e113394a37338a42d8e751e2bef685c5
mahdia7 <mahdiayasmin7800@gmail.com>
2017-08-19 20:44:32 +0600

    75

---
+++ 100+section-class-75/assets/js/main.js
@@ -0,0 +1,107 @@
+(function ($) {
+	"use strict";
+
+	jQuery(document).ready(function ($) {
+		// progress bar
+		$('#bar1').barfiller({
+
+			// color of bar
+			barColor: '#ffffff',
+
+			// shows a tooltip
+			tooltip: true,
+
+			// duration in ms
+			duration: 1000,
+
+			// re-animate on resize
+			animateOnResize: true,
+
+			// custom symbol
+			symbol: "%"
+
+		});
+		$('#bar2').barfiller({
+
+			// color of bar
+			barColor: '#ffffff',
+
+			// shows a tooltip
+			tooltip: true,
+
+			// duration in ms
+			duration: 2000,
+
+			// re-animate on resize
+			animateOnResize: true,
+
+			// custom symbol
+			symbol: "%"
+
+		});
+		$('#bar3').barfiller({
+
+			// color of bar
+			barColor: '#ffffff',
+
+			// shows a tooltip
+			tooltip: true,
+
+			// duration in ms
+			duration: 1000,
+
+			// re-animate on resize
+			animateOnResize: true,
+
+			// custom symbol
+			symbol: "%"
+
+		});
+		$('#bar4').barfiller({
+
+			// color of bar
+			barColor: '#ffffff',
+
+			// shows a tooltip
+			tooltip: true,
+
+			// duration in ms
+			duration: 1000,
+
+			// re-animate on resize
+			animateOnResize: true,
+
+			// custom symbol
+			symbol: "%"
+
+		});
+		$('#bar5').barfiller({
+
+			// color of bar
+			barColor: '#ffffff',
+
+			// shows a tooltip
+			tooltip: true,
+
+			// duration in ms
+			duration: 1000,
+
+			// re-animate on resize
+			animateOnResize: true,
+
+			// custom symbol
+			symbol: "%"
+
+		});
+
+		
+	});
+
+
+	jQuery(window).load(function () {
+
+
+	});
+
+
+}(jQuery));
